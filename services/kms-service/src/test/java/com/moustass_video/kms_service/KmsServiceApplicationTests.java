96d5ea9a366243d42cde331a0d1f559d8a6b5a4f
fehizoro66 <99308058+fehiZ77@users.noreply.github.com>
2026-01-22 16:22:07 +0400

    v3

--- services/kms-service/src/test/java/com/moustass_video/kms_service/KmsServiceApplicationTests.java
+++ services/kms-service/src/test/java/com/moustass_video/kms_service/KmsServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.moustass_video.kms_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class KmsServiceApplicationTests {
 
 	@Test
--- services/transactions-service/src/test/java/com/moustass/transactions_service/TransactionsServiceApplicationTests.java
+++ services/transactions-service/src/test/java/com/moustass/transactions_service/TransactionsServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.moustass.transactions_service;
 
+import com.moustass.transactions_service.client.minio.MinIOService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TransactionsServiceApplicationTests {
+
+	@MockitoBean
+	private MinIOService minIOService;
 
 	@Test
 	void contextLoads() {
