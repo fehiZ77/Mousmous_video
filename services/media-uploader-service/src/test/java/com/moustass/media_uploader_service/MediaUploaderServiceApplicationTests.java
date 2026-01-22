321ddaeee6bf3323ecc2b320f6079da4b0ecf7f1
fehizoro66 <99308058+fehiZ77@users.noreply.github.com>
2026-01-22 16:35:36 +0400

    update build.yml v1

--- services/media-uploader-service/src/test/java/com/moustass/media_uploader_service/MediaUploaderServiceApplicationTests.java
+++ services/media-uploader-service/src/test/java/com/moustass/media_uploader_service/MediaUploaderServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.moustass.media_uploader_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class MediaUploaderServiceApplicationTests {
 
 	@Test
--- services/metadata-service/src/test/java/com/moustass/metadata_service/MetadataServiceApplicationTests.java
+++ services/metadata-service/src/test/java/com/moustass/metadata_service/MetadataServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.moustass.metadata_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class MetadataServiceApplicationTests {
 
 	@Test
