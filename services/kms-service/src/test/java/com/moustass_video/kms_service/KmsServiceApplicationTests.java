eaef6843a1b77178f979c31909ad2597737c18e0
fehizoro66 <99308058+fehiZ77@users.noreply.github.com>
2026-01-24 20:54:52 +0400

    fix build sonar v3

--- services/kms-service/src/test/java/com/moustass_video/kms_service/KmsServiceApplicationTests.java
+++ services/kms-service/src/test/java/com/moustass_video/kms_service/KmsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.moustass_video.kms_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KmsServiceApplicationTests {
 
 	@Test
