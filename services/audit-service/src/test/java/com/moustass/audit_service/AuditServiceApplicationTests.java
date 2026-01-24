ff59204eecf3c3950eb84805392e65ad0132b539
fehizoro66 <99308058+fehiZ77@users.noreply.github.com>
2026-01-24 20:16:12 +0400

    fix build sonar v1

--- services/audit-service/src/test/java/com/moustass/audit_service/AuditServiceApplicationTests.java
+++ services/audit-service/src/test/java/com/moustass/audit_service/AuditServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.moustass.audit_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuditServiceApplicationTests {
 
 	@Test
