c5832edb60795cb8b8abc8cd6499d66d42053e4b
fehizoro66 <99308058+fehiZ77@users.noreply.github.com>
2026-01-26 06:48:41 +0400

    update pom

--- api-gateway/src/test/java/com/daniax/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/daniax/api_gateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,19 @@ package com.daniax.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+    classes = ApiGatewayApplication.class,
+    properties = {
+        "spring.cloud.bootstrap.enabled=false",
+        "spring.main.web-application-type=reactive"
+    }
+)
+@ActiveProfiles("test")
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
